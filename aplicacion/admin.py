8ceda9314e9da04e1931a72b37468e229b59938f
pablo19-alexander <pablo.pass1905@gmai.com>
2022-10-05 21:09:26 -0500

    se concluye el proyecto hasta seleccion de form de negocio

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Tipo_negocio, Usuario, Resultado_cliente
 
 # Register your models here.
+# admin.site.register(Tipo_negocio)
 admin.site.register(Item)
+admin.site.register(Tipo_negocio)
+admin.site.register(Usuario)
+admin.site.register(Resultado_cliente)
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,33 +1,45 @@
+from email.policy import default
 from django.db import models
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import User
 
 # Create your models here.
-tipoNegocio = (
-    (1, 'restaurante'),
-    (2, 'drogueria')
-)
+# tipoNegocio = (
+#     (1, 'restaurante'),
+#     (2, 'drogueria')
+# )
+class Tipo_negocio(models.Model):
+    id = models.AutoField(primary_key=True)
+    tipo_negocio = models.IntegerField(null=True, blank=True, verbose_name="Tipo de Negocio")
+    nombre_tipo_negocio = models.CharField(max_length=100, verbose_name="Nombre del nuevo tipo de negocio", null=True, blank=True)
+    
+    def __str__(self):
+        negocio = "" + self.nombre_tipo_negocio
+        return negocio
 
 class Item(models.Model):
     
     id = models.AutoField(primary_key=True)
-    id_tipo_negocio = models.IntegerField(
-        null=True,
-        choices=tipoNegocio, 
-        default=1,
-        verbose_name="Tipo de Negocio"
-        )
+    tipo_negocio = models.ForeignKey(Tipo_negocio, null=True, blank=True, default=1, on_delete=models.CASCADE)
     descripcion = models.TextField(verbose_name="Descripción del requerimiento", null=True)
     
     def __str__(self):
         item = "DESCRIPCION DEL REQUERIMIENTO: " + self.descripcion
         return item
+# nuevo
+class Usuario(models.Model):
     
-class UserProfile(AbstractUser, PermissionsMixin):
-    email = models.EmailField(
-            ('email address'), 
-            unique=True
-        )
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-    class Meta:
-        abstract = True
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    negocio = models.ForeignKey(Tipo_negocio, null=True, blank=True, default=1, on_delete=models.CASCADE)
+    def __str__(self):
+        user = "El usuario: " + self.usuario.first_name + " - tiene como negocio: " + self.negocio.nombre_tipo_negocio
+        return user
+    
+class Resultado_cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, null=True, blank=True, on_delete=models.CASCADE)
+    requerimiento = models.ForeignKey(Item, null=True, blank=True, on_delete=models.CASCADE)
+    checked = models.BooleanField('checked', default=False)
+    
+    def __str__(self):
+        resultado = "El usuario: " + self.usuario.usuario.first_name + " - requerimiento: " + self.requerimiento.descripcion
+        return resultado
